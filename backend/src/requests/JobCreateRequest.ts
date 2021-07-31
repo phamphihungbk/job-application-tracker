879379aad2e97060da9fc16f60b6c78504f8941c
Hung Pham <phamphihungbk@gmail.com>
2021-07-31 19:29:44 +0800

    done backend

--- backend/src/requests/JobCreateRequest.ts
+++ backend/src/requests/JobCreateRequest.ts
@@ -1,23 +1,23 @@
 import { IsNotEmpty, IsString, MaxLength, IsBoolean } from 'class-validator';
 
 export class JobCreateRequest {
-  @MaxLength(20)
+  @MaxLength(20, { message: 'Title is too long' })
   @IsString()
   position: string;
 
-  @MaxLength(20)
+  @MaxLength(20, { message: 'Title is too long' })
   @IsString()
   company: string;
 
-  @MaxLength(20)
+  @MaxLength(20, { message: 'Title is too long' })
   @IsString()
   status: string;
 
-  @MaxLength(10)
+  @MaxLength(10, { message: 'Title is too long' })
   @IsString()
   location: string;
 
-  @MaxLength(10)
+  @MaxLength(10, { message: 'Title is too long' })
   @IsString()
   requirements: string;
 
